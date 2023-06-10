173a864b03ebe55b15694b80dab5865de2919593
Freskim Aliu <freskimaliu@gmail.com>
2023-06-11 01:40:02 +0200

    Small update in App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fibonacci Calculator V2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
